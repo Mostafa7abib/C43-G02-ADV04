d64b41616af8aaa4fcae63e927bcb4cec266edad
Mostafa7abib <mostafamohabib1234@gmail.com>
2025-01-22 14:44:16 +0200

    Video 1 - Delegate_Example01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace Demo
 {
+    public delegate int StringFuncDelegate(string str);
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate-Example1
+            StringFuncDelegate reference;
+            reference = new StringFuncDelegate(String_Functions.GetCountOfUpperCaseChars);
+            reference -= new StringFuncDelegate(String_Functions.GetCountOfLowerCaseChars);
+            //reference = String_Functions.GetCountOfUpperCaseChars; //Syntsx Sugar
+            int count = reference.Invoke("Mostafa MOHamed"); 
+            // int count = reference("Hello World"); //Syntsx Sugar
+            Console.WriteLine(count);
+            #endregion
         }
     }
 }
